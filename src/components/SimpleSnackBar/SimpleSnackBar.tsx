28a69b58085ea291e3e7127e03da9313fd5c3d6c
Ido Ben Zaken <idobz1234567890@gmail.com>
2022-01-12 22:12:00 +0200

    refactored spinner stylings

--- src/components/SimpleSnackBar/SimpleSnackBar.tsx
+++ src/components/SimpleSnackBar/SimpleSnackBar.tsx
@@ -3,6 +3,7 @@ import Snackbar from "@mui/material/Snackbar";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import { useSnackBarContext } from "../../store/snackbar";
+import "./SimpleSnackBar.scss";
 
 interface ISnackbar {
   message?: string;
